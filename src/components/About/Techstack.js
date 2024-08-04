d8a768d2217e11ff7631c7cf13bead2356333ffe
abhishekraj15 <rabhi0766@gmail.com>
2024-08-04 09:43:17 +0530

    added Tooltip

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,59 +1,37 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
-import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-  DiJava,
-} from "react-icons/di";
+import { DiJavascript1, DiReact, DiNodejs, DiMongodb, DiGit } from "react-icons/di";
 import { FaAws } from "react-icons/fa";
-import {
-  SiRedis,
-  SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
-} from "react-icons/si";
-import { TbBrandGolang, TbBrandNextjs, TbBrandTailwind } from "react-icons/tb";
+import { TbBrandNextjs, TbBrandTailwind } from "react-icons/tb";
+import { SiFirebase } from "react-icons/si";
+
+const techs = [
+  { icon: <CgCPlusPlus />, name: "C++" },
+  { icon: <TbBrandTailwind />, name: "Tailwind CSS" },
+  { icon: <DiJavascript1 />, name: "JavaScript" },
+  { icon: <DiReact />, name: "React" },
+  { icon: <TbBrandNextjs />, name: "Next.js" },
+  { icon: <DiNodejs />, name: "Node.js" },
+  { icon: <DiMongodb />, name: "MongoDB" },
+  { icon: <DiGit />, name: "Git" },
+  { icon: <SiFirebase />, name: "Firebase" },
+  { icon: <FaAws />, name: "AWS" },
+];
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandTailwind />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandNextjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaAws />
-      </Col>
+      {techs.map((tech, idx) => (
+        <Col xs={4} md={2} className="tech-icons" key={idx}>
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip id={`tooltip-${idx}`}>{tech.name}</Tooltip>}
+          >
+            <div>{tech.icon}</div>
+          </OverlayTrigger>
+        </Col>
+      ))}
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,5 +1,39 @@
+// import React from "react";
+// import { Col, Row } from "react-bootstrap";
+// import {
+//   SiVisualstudiocode,
+//   SiPostman,
+//   SiVercel,
+//   SiMacos,
+//   SiJira,
+// } from "react-icons/si";
+
+// function Toolstack() {
+//   return (
+//     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiMacos />
+//       </Col>
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiVisualstudiocode />
+//       </Col>
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiPostman />
+//       </Col>
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiJira />
+//       </Col>
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiVercel />
+//       </Col>
+//     </Row>
+//   );
+// }
+
+// export default Toolstack;
+
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, OverlayTrigger, Tooltip } from "react-bootstrap";
 import {
   SiVisualstudiocode,
   SiPostman,
@@ -8,24 +42,27 @@ import {
   SiJira,
 } from "react-icons/si";
 
+const tools = [
+  { icon: <SiMacos />, name: "macOS" },
+  { icon: <SiVisualstudiocode />, name: "Visual Studio Code" },
+  { icon: <SiPostman />, name: "Postman" },
+  { icon: <SiJira />, name: "Jira" },
+  { icon: <SiVercel />, name: "Vercel" },
+];
+
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiJira />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
+      {tools.map((tool, idx) => (
+        <Col xs={4} md={2} className="tech-icons" key={idx}>
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip id={`tooltip-${idx}`}>{tool.name}</Tooltip>}
+          >
+            <div>{tool.icon}</div>
+          </OverlayTrigger>
+        </Col>
+      ))}
     </Row>
   );
 }
