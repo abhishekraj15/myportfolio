f268023952b42a707148ae8f9c43e4b96fa49399
abhishekraj15 <rabhi0766@gmail.com>
2024-08-04 12:49:06 +0530

    fix

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Abhishek Rajput </span>
-            from <span className="purple"> Bijnor, India.</span>
+            from <span className="purple"> Noida, India.</span>
             <br />
             I am currently employed as a Frontend developer at devlupers.
             <br />
@@ -32,7 +32,7 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "One day developed something different!"{" "}
+            "One day developing something different!"{" "}
           </p>
           <footer className="blockquote-footer">Abhishek</footer>
         </blockquote>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -20,7 +20,7 @@ function Home2() {
             </h1>
             <p className="home-about-body">
               I fell in love with Development and I have at least learnt
-              something, I think… 🤷‍♂️
+              something, I think… 💬
               <br />
               <br />I am fluent in
               <i>
@@ -29,16 +29,12 @@ function Home2() {
                   C++, Javascript, React.js and Nextjs .
                 </b>
               </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
+              My field of Interests are building new &nbsp;
               <i>
                 <b className="purple">Web Technologies and Products </b> and
                 also in areas related to{" "}
                 <b className="purple">Full Stack Development.</b>
               </i>
-              <br />
-              <br />
               Whenever possible, I also apply my passion for developing websites
               with <b className="purple">Node.js and Express.js</b> and
               <i>
