9db680ed49f51865d7eb0d466c5a87efe46df59e
abhishekraj15 <rabhi0766@gmail.com>
2024-08-11 22:15:23 +0530

    added

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Abhishek Rajput </span>
-            from <span className="purple"> Noida, India.</span>
+            from <span className="purple "> Noida, India.</span>
             <br />
             I am currently employed as a Frontend developer at devlupers.
             <br />
